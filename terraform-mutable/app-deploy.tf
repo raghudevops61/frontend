ec5c65ff1a74920f49fc7e6bab33df2f40111b8c
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-19 06:47:21 +0530

    Update roboshop/frontend/

---
+++ terraform-mutable/app-deploy.tf
@@ -0,0 +1,15 @@
+resource "null_resource" "app-deploy" {
+  count = length(aws_spot_instance_request.ec2-spot)
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_USERNAME"]
+      password = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_PASSWORD"]
+      host     = aws_spot_instance_request.ec2-spot.*.private_ip[count.index]
+    }
+
+    inline = [
+      "ansible-pull -U https://github.com/raghudevopsb61/ansible.git roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV}"
+    ]
+  }
+}
--- terraform-mutable/data.tf
+++ terraform-mutable/data.tf
@@ -22,3 +22,11 @@ data "terraform_remote_state" "alb" {
   }
 }
 
+data "aws_secretsmanager_secret" "common" {
+  name = "common/ssh"
+}
+
+data "aws_secretsmanager_secret_version" "secrets" {
+  secret_id = data.aws_secretsmanager_secret.common.id
+}
+
