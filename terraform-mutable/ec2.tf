7ebedc235813e1098bc093e2525ca1015084703c
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-19 06:16:28 +0530

    Update roboshop/frontend/

--- terraform-mutable/ec2.tf
+++ terraform-mutable/ec2.tf
@@ -29,3 +29,14 @@ resource "aws_lb_target_group_attachment" "tg-attach" {
   target_id        = aws_spot_instance_request.ec2-spot.*.spot_instance_id[count.index]
   port             = 80
 }
+
+resource "aws_lb_listener" "lb-listener" {
+  load_balancer_arn =
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
